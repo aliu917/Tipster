9a11c31133961fbde1104cb9de7914ebcc9b5598
Angela Liu <aliu18@fb.com>
2019-06-25 13:57:51 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by aliu18 on 6/25/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by aliu18 on 6/25/19.
+//  Copyright © 2019 aliu18. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
