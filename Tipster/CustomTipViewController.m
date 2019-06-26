6f1fb1ff5cde8c2295773ab4055216941c6e0845
Angela Liu <aliu18@fb.com>
2019-06-26 10:05:22 -0700

    Semi working.

--- Tipster/CustomTipViewController.m
+++ Tipster/CustomTipViewController.m
@@ -18,17 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setBool: NO forKey: @"useCustom"];
+    //[defaults setBool: NO forKey: @"useCustom"];
     // Do any additional setup after loading the view.
 }
 
 - (IBAction)updateUserDefault:(id)sender {
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     double tip = [self.customTipField.text doubleValue];
-    [defaults setDouble: tip forKey:@"custom_tip_percentage"];
+    [defaults setDouble: tip / 100 forKey:@"custom_tip_percentage"];
     [defaults setBool: YES forKey: @"useCustom"];
     [defaults synchronize];
     NSLog(@"Updated user defaults.");
+    if ([defaults boolForKey:@"useCustom"]) {
+        NSLog(@"Now using custom tip.");
+    }
 }
 
 /*
--- Tipster/ViewController.m
+++ Tipster/ViewController.m
@@ -25,15 +25,18 @@
     // Do any additional setup after loading the view.
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     double useCustom = [defaults boolForKey:@"useCustom"];
+    [defaults setDouble: [self getTipSelected] forKey: @"prevTip"];
+    [defaults synchronize];
     
-    if (NO) {
-        if (useCustom) {
-            NSLog(@"Trying to use custom.");
-            NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-            self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", [defaults doubleForKey:@"custom_tip_percentage"]];
-            self.customTipText.text = @"Using custom tip.";
-        }
+    
+    //if (NO) {
+    if (useCustom) {
+        NSLog(@"Trying to use custom.");
+        NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+        self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", [defaults doubleForKey:@"custom_tip_percentage"] * [self.billField.text doubleValue]];
+        self.customTipText.text = @"Using custom tip.";
     }
+    //}
 }
 
 - (IBAction)onTap:(id)sender {
@@ -41,23 +44,36 @@
     [self.view endEditing:YES];
 }
 
+- (double)getTipSelected {
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    return tipPercentage;
+}
+
 //Editing bill
 - (IBAction)onEdit:(id)sender {
     
     double bill = [self.billField.text doubleValue];
     
-    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
-    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
-    
+    double tipPercentage = [self getTipSelected];
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    double prevTip = [defaults doubleForKey:@"prevTip"];
+    if (tipPercentage != prevTip) {
+        [defaults setDouble: tipPercentage forKey: @"prevTip"];
+        [defaults setBool: NO forKey: @"useCustom"];
+        self.customTipText.text = @"Using tip selected.";
+        [defaults synchronize];
+
+    }
+    
     double useCustom = [defaults boolForKey:@"useCustom"];
     
-    if (NO) {
+    //if (NO) {
         if (useCustom) {
             NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
             tipPercentage = [defaults doubleForKey:@"custom_tip_percentage"];
         }
-    }
+    //}
     
     double tip = tipPercentage * bill;
     double total = bill + tip;
